be7f5e07041e69bf3deb623ac639788249333c7b
Nick Sergeant <nsergeant@hubspot.com>
2020-07-21 22:27:08 -0400

    Lots of stuff

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
-const ACCELERATION = 10
-const FRICTION = 10
+const ACCELERATION = 500
+const FRICTION = 500
 const MAX_SPEED = 80
 
 var velocity = Vector2.ZERO
@@ -49,9 +49,8 @@ func _physics_process(delta):
         input_vector = move_by_keyboard()
     
     if input_vector != Vector2.ZERO:
-        velocity += input_vector * ACCELERATION * delta
-        velocity = velocity.clamped(MAX_SPEED * delta)
+        velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
     else:
         velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-        
-    move_and_collide(velocity)
+    
+    velocity = move_and_slide(velocity)
